01283df17e721e279b0aa2e2193c62b10231135e
YolandaThant <yolanda.lee159@gmail.com>
2024-09-25 15:52:56 -0400

    hi

--- src/main/java/org/translation/JSONTranslator.java
+++ src/main/java/org/translation/JSONTranslator.java
@@ -19,7 +19,8 @@ import org.json.JSONObject;
 public class JSONTranslator implements Translator {
 
     // TODO Task: pick appropriate instance variables for this class
-    private final Map<String, Map<String, String>> translations;
+    private final JSONArray jsonArray;
+    private final String ALPHA3 = "alpha3";
     /**
      * Constructs a JSONTranslator using data from the sample.json resources file.
      */
@@ -37,26 +38,9 @@ public class JSONTranslator implements Translator {
         // read the file to get the data to populate things...
         try {
             String jsonString = Files.readString(Paths.get(getClass().getClassLoader().getResource(filename).toURI()));
-            JSONArray jsonArray = new JSONArray(jsonString);
-
+            this.jsonArray = new JSONArray(jsonString);
             // y TODO Task: use the data in the jsonArray to populate your instance variables
             //            Note: this will likely be one of the most substantial pieces of code you write in this lab.
-            translations = new HashMap<>();
-            for (int i = 0; i < jsonArray.length(); i++) {
-                JSONObject jsonObject = jsonArray.getJSONObject(i);
-                String countryCode = jsonObject.getString("alpha3");
-
-                // new Map to store lang trans for this country
-                Map<String, String> languageMap = new HashMap<>();
-                for (String key : jsonObject.keySet()) {
-                    if (!("id".equals(key) || "alpha2".equals(key) || "alpha3".equals(key))) {
-                        //                        languageMap.put(key, jsonObject.getString(key));
-                        languageMap.put(key, jsonObject.optString(key, null));
-                    }
-                }
-                translations.put(countryCode, languageMap);
-                // System.out.println(translations);
-            }
         }
         catch (IOException | URISyntaxException ex) {
             throw new RuntimeException(ex);
@@ -67,31 +51,49 @@ public class JSONTranslator implements Translator {
     public List<String> getCountryLanguages(String country) {
         // y TODO Task: return an appropriate list of language codes,
         //            but make sure there is no aliasing to a mutable object
-        Map<String, String> languageMap = translations.get(country);
-        // System.out.println(languageMap);
-        if (languageMap == null) {
-            return new ArrayList<>();
+        List<String> languages = new ArrayList<>();
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+            if (jsonObject.getString(ALPHA3).equalsIgnoreCase(country)) {
+                for (String key : jsonObject.keySet()) {
+                    if (!("id".equals(key) || "alpha2".equals(key) || ALPHA3.equals(key))) {
+                        languages.add(key);
+                    }
+                }
+                break;
+            }
         }
-        List<String> languages = new ArrayList<>(languageMap.keySet());
-        System.out.println("Languages for country " + country + ": " + languageMap);
-        System.out.println(languageMap.keySet());
-        return new ArrayList<>(languageMap.keySet());
+        return languages;
     }
 
     @Override
     public List<String> getCountries() {
         // y TODO Task: return an appropriate list of country codes,
         //            but make sure there is no aliasing to a mutable object
-        return new ArrayList<>(translations.keySet());
+        ArrayList<String> countries = new ArrayList<>();
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject line = jsonArray.getJSONObject(i);
+            countries.add(line.getString(ALPHA3));
+        }
+        return countries;
     }
 
     @Override
     public String translate(String country, String language) {
         // y TODO Task: complete this method using your instance variables as needed
-        Map<String, String> languageMap = translations.get(country);
-        if (languageMap != null && languageMap.containsKey(language)) {
-            return languageMap.get(language);
+        String result = "Not found";
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject line = jsonArray.getJSONObject(i);
+            if (line.getString(ALPHA3).equalsIgnoreCase(country)) {
+                if (line.has(language.toLowerCase())) {
+                    result = line.getString(language.toLowerCase());
+                }
+                else {
+                    result = "Not available";
+                }
+            }
         }
-        return null;
+        return result;
     }
 }
